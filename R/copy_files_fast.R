daee149b48f7efb0c723bd8cf08f633e05b40f1e
IDOJI <ido.ji@proton.me>
2022-12-03 21:14:35 +0900

    Update copy_files_fast.R

--- R/copy_files_fast.R
+++ R/copy_files_fast.R
@@ -5,10 +5,10 @@ copy_files_fast = function(Source.Folder, Destination.Folder, move=F, overwrite=
     cmd = paste("robocopy",
                 Source.Folder,    # Source Folder
                 Destination.Folder,    # Destination Folder
-                "/E /MOVE /MT:20 /IS /Log:c:\\temp\\Robolog.log",
+                "/E /MOVE /MT:20 /is /it",
                 sep = " ")
     system(cmd) %>% invisible()
-    cat("\n",crayon::green("Moviing files is done!"),"\n")
+    cat("\n",crayon::green("Moving files is done!"),"\n")
   }else if(move && !overwrite){
     cmd = paste("robocopy",
                 Source.Folder,    # Source Folder
@@ -16,7 +16,7 @@ copy_files_fast = function(Source.Folder, Destination.Folder, move=F, overwrite=
                 "/E /MOVE /MT:20 /Log:c:\\temp\\Robolog.log",
                 sep = " ")
     system(cmd) %>% invisible()
-    cat("\n",crayon::green("Moviing files is done!"),"\n")
+    cat("\n",crayon::green("Moving files is done!"),"\n")
   }else if(!move && overwrite){
     cmd = paste("robocopy",
                 Source.Folder,    # Source Folder
