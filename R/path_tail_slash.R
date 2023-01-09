a4bf646609d21ea3a2ff5095f2633d3041fb687e
Ido Ji <ido.ji@proton.me>
2023-01-09 13:09:57 +0900

    Update path_tail_slash.R

--- R/path_tail_slash.R
+++ R/path_tail_slash.R
@@ -1,9 +1,19 @@
-path_tail_slash = function(path){
-  for(i in 1:length(path)){
-    ith_path = path[i]
-    if(substr(ith_path, start=nchar(ith_path), stop=nchar(ith_path)) != "/"){
-      path[i] = paste(ith_path, "/", sep="")
+path_tail_slash = function(path, add_slash=TRUE){
+  path_new = path
+  if(add_slash){
+    for(i in 1:length(path)){
+      ith_path = path[i]
+      if(substr(ith_path, start=nchar(ith_path), stop=nchar(ith_path)) != "/"){
+        path_new[i] = paste(ith_path, "/", sep="")
+      }
+    }
+  }else{
+    for(i in 1:length(path)){
+      ith_path = path[i]
+      if(substr(ith_path, start=nchar(ith_path), stop=nchar(ith_path)) == "/"){
+        path_new[i] = substr(ith_path, start=1, stop=nchar(ith_path)-1)
+      }
     }
   }
-  return(path)
+  return(path_new)
 }
