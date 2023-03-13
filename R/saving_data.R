d718c695953ea14b88cf71047afd26176111b61a
Ido Ji <ido.ji@proton.me>
2023-03-13 18:35:40 +0900

    change saving data code

--- R/saving_data.R
+++ R/saving_data.R
@@ -1,10 +1,11 @@
 saving_data = function(rda.name, rda, path){
   path = path %>% path_tail_slash
-  file = paste0(path, rda.name)
-  assign(rda.name, rda)
-  require(usethis)
-  do.call("use_data", list(as.name(rda.name), overwrite=TRUE))
+  # file = paste0(path, rda.name)
+  # assign(rda.name, rda)
+  # require(usethis)
+  # do.call("use_data", list(as.name(rda.name), overwrite=TRUE))
 
+  save(rda, file = paste0(path, rda.name, ".RData"))
   # usethis::use_data(data.df, overwrite=T)
   # usethis::use_data_raw(rda.name)
   cat("\n", crayon::blue("Exporting"), crayon::red(rda.name), crayon::blue("is done!"),"\n")
