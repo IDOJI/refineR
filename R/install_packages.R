a28487b651d83a4038b7fd6d3aa84502296eef9e
Ido Ji <ido.ji@proton.me>
2023-08-11 20:17:48 +0900

    copy files

--- R/install_packages.R
+++ R/install_packages.R
@@ -1,9 +1,15 @@
-install_packages = function(package_names){
-  sapply(package_names, FUN=function(x){
-    if(! x %in% installed.packages()){
-      install.packages(x)
+install_packages = function(packages, load=TRUE) {
+  # load : load the packages after installation?
+
+  for(pkg in packages) {
+    if (!require(pkg, character.only = TRUE)) {
+      install.packages(pkg)
     }
-  })
+
+    if(load){
+      library(pkg, character.only = TRUE)
+    }
+  }
 }
 
 
