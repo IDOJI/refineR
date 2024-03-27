42d017da1388a12bab34a7c8b2eeb16222c866d1
IDOJI <ido.ji@proton.me>
2024-03-27 21:15:37 +0900

    Update change_class.R

--- R/change_class.R
+++ R/change_class.R
@@ -1,29 +1,17 @@
-change_class = function(data.list, which.col, what.class){
-  ## 클래스 바꾸기
-  if(is.data.frame(data.list)){
-    is.df = T
-    data.list = list(data.list)
-  }else{
-    is.df = F
+change_class = function(df, cols, what_class = c("numeric", "integer", "factor", "logical", "complex", "raw", "Date"), strict=TRUE) {
+  # class
+  what_class = tolower(what_class)
+    
+  # new df
+  new_df = df %>% as.data.frame
+  
+  # change class for each col
+  for(ith_col in cols){
+    new_df[[ith_col]] = as(new_df[[ith_col]], what_class, strict)
   }
-  for(i in 1:length(data.list)){
-    # i=1
-    for(j in 1:length(which.col)){
-      selected.col = data.list[[i]][,which.col[j]] %>% unlist
-      if(what.class %in% c("character", "Character", "Char", "char", "c",  "C")){
-        selected.col = as.character(selected.col) %>% suppressWarnings
-        selected.col = stringr::str_trim(selected.col)
-      }else if(what.class %in% c("numeric", "num", "Num", "Numeric", "N")){
-        selected.col = as.numeric(selected.col) %>% suppressWarnings
-      }else if(what.class %in% c("integer","Integer", "int", "Int", "I")){
-        selected.col = as.integer(selected.col) %>% suppressWarnings
-      }
-      data.list[[i]][,which.col[j]] = selected.col
-    }
-  }
-  if(is.df){
-    return(data.list[[1]])
-  }else{
-    return(data.list)
-  }
+  
+  
+  # 변환된 데이터 프레임 반환
+  return(new_df)
 }
+
